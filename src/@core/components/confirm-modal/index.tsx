f48d01a0d844a8e0fc1714647afdd7362dc0c029
Đàm Hoàng Anh <damhoanganh@ams-MacBook-Pro.local>
2022-12-31 17:28:04 +0700

    fix responsive modal confirm

--- src/@core/components/confirm-modal/index.tsx
+++ src/@core/components/confirm-modal/index.tsx
@@ -4,8 +4,6 @@ import DialogActions from '@mui/material/DialogActions'
 import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
-import useMediaQuery from '@mui/material/useMediaQuery'
-import { useTheme } from '@mui/material/styles'
 
 interface Props {
   open: boolean
@@ -17,11 +15,8 @@ interface Props {
 const ConfirmModal = (props: Props) => {
   const { open, onClose, title, onConfirm } = props
 
-  const theme = useTheme()
-  const fullScreen = useMediaQuery(theme.breakpoints.down('md'))
-
   return (
-    <Dialog fullScreen={fullScreen} open={open} onClose={onClose} aria-labelledby='responsive-dialog-title'>
+    <Dialog open={open} onClose={onClose} aria-labelledby='responsive-dialog-title'>
       <DialogTitle id='responsive-dialog-title'>{title || 'Xoá'}</DialogTitle>
       <DialogContent>
         <DialogContentText>Hành động này không thể hoàn tác. Bạn có chắc chắn muốn xoá không?</DialogContentText>
--- src/apiClient/axiosClient.ts
+++ src/apiClient/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import authConfig from 'src/configs/auth'
 
-const getLocalStorage = (key: string) => {
+export const getTokenLocalStorage = (key: string) => {
   if (typeof window !== 'undefined') {
     return window.localStorage.getItem(key)
   }
@@ -11,20 +11,15 @@ const axiosClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
-    Authorization: 'Bearer ' + getLocalStorage(authConfig.storageTokenKeyName)
+    Authorization: 'Bearer ' + getTokenLocalStorage(authConfig.storageTokenKeyName)
   }
 })
 
-// Add a response interceptor
 axiosClient.interceptors.response.use(
   function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
     return Promise.reject(error)
   }
 )
